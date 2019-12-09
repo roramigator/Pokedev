36448f9245223f6dd07db1fcc586c6903d3c02fe
roramigator <roramigator@gmail.com>
2019-12-09 14:18:43 -0500

    Stable version

--- src/master.js
+++ src/master.js
@@ -1,15 +1,1 @@
 import "./art.scss";
-const IS = require('./lib/interactionSystem');
-
-
-
-
-// api request
-// fetch("https://pokeapi.co/api/v2/pokemon/").then(r=>r.json()).then(res=>{
-//   console.log(res);
-//});
-//
-
-
-
-// https://assets.pokemon.com/assets/cms2/img/pokedex/full/{001-809}.png -> images
