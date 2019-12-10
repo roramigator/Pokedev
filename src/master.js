d888c8bee3ad16c102b23e5415f4fd2441492f67
roramigator <roramigator@gmail.com>
2019-12-10 15:25:44 -0500

    Stable

--- src/master.js
+++ src/master.js
@@ -3,7 +3,6 @@ import "./art.scss";
 const IS = require('./lib/interactionSystem');
 const GE = require('./lib/graphicEngine');
 
-//  ---
 //  LOAD MENU ON SCREEN
 GE.setMenu(IS.select(".screen"));
 //  ---
